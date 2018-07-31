3bf05a2bc3272245dfde8c11f0d42a9ece8fb86c
Denis <papa149@mail.ru>
2018-07-31 14:19:35 +0500

    Initial commit

---
+++ src/main/java/com/tedBilgar/StudentHelper2.java
@@ -0,0 +1,20 @@
+package com.tedBilgar;
+
+public class StudentHelper2 {
+    private String message;
+
+    public StudentHelper2() {
+    }
+
+    public StudentHelper2(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
